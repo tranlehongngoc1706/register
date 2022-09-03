cce1bd484fdc52e092b3e862b295903c0025d8b3
tranlehongngoc1706 <102713164+tranlehongngoc1706@users.noreply.github.com>
2022-09-03 08:11:27 -0500

    Update customer_register.php

--- www/customer_register.php
+++ www/customer_register.php
@@ -1,80 +1,101 @@
-<?php 
-require_once('same_register_2.php')
+<?php
+    require_once('same_register_2.php');
 ?>
 <?php
     $your_namephpErr= $your_addressphpErr = "";
     $your_namephp = $your_addressphp = "";
+    $success_message = "";
 
     if($_SERVER["REQUEST_METHOD"] == "POST") {
-        // Form validation
-        if ($finduser == true) {
-            echo "";
-        }  else { 
-        $myfile2 = fopen("C:/fullstack/account.db","r");
-        $finduser2 = false;
-        while (!feof($myfile2))
+        // Check local uniqueness
+        $file2=fopen("C:/fullstack/name.db","r");
+        $find_name=false;
+        while(!feof($file2))
         {
-            $line2 = fgets($myfile2);
-            $array2 = explode (",", $line2);
-            if (trim($array2[3]) == $_POST["your_name"]) {
-                $finduser2 = true;
-                break;
+            $line2 = fgets($file2);
+            $array2 = explode(",",$line2);
+            if(trim($array2[0]) == $_POST["your_name"])
+            {
+                $find_name=true;
             }
         }
-        fclose($myfile2);
-
-        if ($finduser2 == true) {
-            echo "Your name is similar to another one. Please register again"."\n";
-        } else {
-            if (empty($_POST["your_name"])) {
-                $your_namephpErr = "Your name must not be empty";
-            }
-            else{
-                $your_namephp = test_input($_POST["your_name"]);
-                if (strlen($_POST["your_name"]) < 5) {
-                    $your_namephpErr = "Your name must contain at least 5 characters";
-                } 
-                else {
-                    echo "Your name:". $your_namephp;
-                    echo "<br>";
-                }
-            }    
-            $myfile3 = fopen("C:/fullstack/account.db","r");
-            $finduser3 = false;
-            while (!feof($myfile3))
+        fclose($file2);
+        
+        $file3=fopen("C:/fullstack/address.db","r");
+        $find_address=false;
+        while(!feof($file3))
+        {
+            $line3 = fgets($file3);
+            $array3 = explode(",",$line3);
+            if(trim($array3[0]) == $_POST["your_address"])
             {
-            $line3 = fgets($myfile3);
-            $array3 = explode (",", $line3);
-            if (trim($array3[4]) == $_POST["your_address"]) {
-                $finduser3 = true;
-                break;
+                $find_address=true;
             }
-            }
-            fclose($myfile3);
-
-            if ($finduser3 == true) {
-                echo "Your name is similar to another one. Please register again"."\n";
-            } else {
-                if (empty($_POST["your_address"])) {
-                    $your_addressphpErr = "Your address must not be empty";
-                }
-                else{
-                    $your_addressphp = test_input($_POST["your_address"]);
-                    if (strlen($_POST["your_address"]) < 5) {
-                    $your_addressphpErr = "Your name must contain at least 5 characters";
+        }
+            fclose($file3);
+        
+            if ($find_name == true) {
+                echo "Already existed name. Please register again.";
+                echo "<br>";
+            } 
+        
+            if ($find_address == true) {
+                echo "Already existed address. Please register again.";
+            } 
+                
+            else {
+                    
+                // Display user's input
+                echo "<h1> Registration Information </h1>";
+                echo "Username:".$usernamephp;
+                echo "<br>";
+                echo "Password:".$passwordphp;
+                echo "<br>";
+                echo "Profile picture name:".$_FILES["image"]["name"];
+                echo "<br>";
+        
+                // Form validation
+                if (empty($_POST["your_name"])) {
+                    $your_namephpErr = "Your name must not be empty";
+                } else{
+                    $your_namephp = test_input($_POST["your_name"]);
+                    if (strlen($_POST["your_name"]) < 5) {
+                        $your_namephpErr = "Your name must contain at least 5 characters";
+                    } 
+                    else {
+                        echo "Your name:".$your_namephp;
+                        echo "<br>";
                     }
                 }
-
-                // Display user's input
-                echo "Your address:".$your_addressphp;
-
-                // File handling
-                $file = fopen("C:/fullstack/account.db", "a") or die("Unable to open file!");
-                fwrite($file, "\n". $usernamephp.",".$passwordphphash.",".$target_file.",".$your_namephp.",".$your_addressphp);
-                fclose($file);
+        
+                if (empty($_POST["your_address"])) {
+                    $your_addressphpErr = "Your address must not be empty";
+                } else{
+                    $your_addressphp = test_input($_POST["your_address"]);
+                    if (strlen($_POST["your_address"]) < 5) {
+                        $your_namephpErr = "Your address must contain at least 5 characters";
+                    } 
+                    else {
+                        echo "Your address:".$your_addressphp;
+                        echo "<br>";
+                    }
+                }
+                    if ($finduser == false && $find_address == false && $find_name == false) {
+                    // File handling
+                    $file = fopen("C:/fullstack/account.db", "a") or die("Unable to open file!");
+                    fputs($file,($usernamephp.";".$passwordphphash.";".$_FILES['image']['name'].";"."no".";"."no".";".$your_namephp.";".$your_addressphp.";"."no".";"."customer"."\r\n"));
+                    fclose($file);
+                        
+                    $file2 = fopen("C:/fullstack/name.db", "a+");
+                    fputs($file2,($your_namephp."\r\n"));
+                    fclose($file2);
+        
+                    $file3 = fopen("C:/fullstack/address.db", "a+");
+                    fputs($file3,($your_addressphp."\r\n"));
+                    fclose($file3);
+                    $success_message ='Registered successfully!';
                 }
             }
-        }
     }
 ?>
 
@@ -115,9 +136,10 @@ require_once('same_register_2.php')
         </div>
            
         <div class="form_row">
-            <input type="submit" name= "act" class="btn" value= "Register now">
+            <input type="submit" name= "act" class="btn" value= "Register now"> <br>
+            <span class="message-success"><?php echo $success_message ?></span> <br>
         </div>
-
+        <input type="hidden" name="customer">
         <p> Already have an account? <a href= "login.php"> Login now </a> </p>
     </form>
 </div>
